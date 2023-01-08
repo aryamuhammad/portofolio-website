b9677e427ed8a2a12c050c03666f0b495a0054d1
Muhammad Arya Wirawan <maryawirawan13@gmail.com>
2023-01-08 21:40:43 +0700

    fixing bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../context/ThemeProvider'
 function Button({text,addStyle}) {
   const {isDark} = useContext(ThemeContext)
   return (
-    <a target={"_blank"}><button type="button" className={`text-white bg-gradient-to-r ${isDark? "from-cyan-400 to-sec-color-dark":"from-cyan-400 to-sec-color-white"} from-cyan-400 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 hover:-translate-y-1 transition-all duration-300 ${addStyle}`}>{text}</button></a>
+    <a target={"_blank"} href="https://wa.me/+6289693210097"><button type="button" className={`text-white bg-gradient-to-r ${isDark? "from-cyan-400 to-sec-color-dark":"from-cyan-400 to-sec-color-white"} from-cyan-400 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 hover:-translate-y-1 transition-all duration-300 ${addStyle}`}>{text}</button></a>
   )
   }
 export default Button
