60ae14cc05d2639557d3e3119e4f59c1b7c684b6
Muhammad Arya Wirawan <maryawirawan13@gmail.com>
2023-01-08 20:56:16 +0700

    fixing bug

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -3,6 +3,9 @@ import React, { createContext, useState } from "react";
 export const ThemeContext = createContext();
 function ThemeProvider({children}) {
   const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem("isDark?")));
+  if(localStorage.getItem("isDark?") === null) {
+    localStorage.setItem("isDark?", true)
+  }
   localStorage.setItem("isDark?", JSON.stringify(isDark))
   return <ThemeContext.Provider value={{isDark, setIsDark}}>
     {children}
